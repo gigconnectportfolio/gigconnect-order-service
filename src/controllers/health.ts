0e3ced46b2c39e855f33ca4cfd791194f149d39f
Keith <karirukeith@gmail.com>
2025-11-06 18:30:07 +0300

    minor tweaks

--- src/controllers/health.ts
+++ src/controllers/health.ts
@@ -2,9 +2,9 @@ import {Request, Response} from "express";
 import {StatusCodes} from "http-status-codes";
 
 /*
-    This function serves as a health check endpoint for the Chat Service.
+    This function serves as a health check endpoint for the Order Service.
     It responds with a 200 OK status and a message indicating that the service is healthy.
  */
 export function health(_req: Request, res: Response): void {
-    res.status(StatusCodes.OK).send('Chats Service is healthy');
+    res.status(StatusCodes.OK).send('Order Service is healthy');
 }
